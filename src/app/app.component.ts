ce9991ea27c4b6f1160bef5df65602541418393b
Matias Corta <matiascorta999@gmail.com>
2023-09-05 19:31:37 -0300

    primera tarea

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
+  edadUno: number = 0;
+  edadDos: number = 0;
+  promedio: number = 0;
+  suma: number = 0;
+
+  calcular() {
+    this.promedio = (this.edadUno + this.edadDos) / 2;
+    this.suma = this.edadUno + this.edadDos;
+  }
+
+  limpiar() {
+    this.edadUno = 0;
+    this.edadDos = 0;
+    this.promedio = 0;
+    this.suma = 0;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BindingComponent } from './components/binding/binding.component';
+import { BienvenidoComponent } from './components/bienvenido/bienvenido.component';
+import { LoginComponent } from './components/login/login.component';
+import { ErrorComponent } from './components/error/error.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BindingComponent
+    BindingComponent,
+    BienvenidoComponent,
+    LoginComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
